0e962ac0aaba872261be8c66a683490df99126be
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-03-16 11:42:06 +0300

    Initial commit

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// TODO: init confi: cleanenv
+
+	// TODO: ini logger: slog
+
+	// TODO: init storage: sqlite3
+
+	// TODO: init router: chi, chi render
+
+	// TODO: run server
+}
